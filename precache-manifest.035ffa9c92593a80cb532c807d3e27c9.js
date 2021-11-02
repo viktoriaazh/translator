99873e78d5c632622a31b4a2fda9b817037cce09
viktoriaazh <85249067+viktoriaazh@users.noreply.github.com>
2021-11-02 23:12:20 +0300

    Add files via upload

---
+++ precache-manifest.035ffa9c92593a80cb532c807d3e27c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54acb343c8fb595ab7f3156dff0daaa4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f4d2dbdf03207fa514d",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "8ae49aac9dab507a9ba8",
+    "url": "/static/js/2.18b2a2a8.chunk.js"
+  },
+  {
+    "revision": "4f4d2dbdf03207fa514d",
+    "url": "/static/js/main.0ca1f2b2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
